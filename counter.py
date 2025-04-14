54198dee7740566865a9707e218e824de3b59e06
Shawn Allen <Shawn.Allen@vu.com>
2025-04-14 16:24:26 -0500

    Takes file name input, loops through each line and gets how many emails each person has sent. Stores that info in a definition. Prints email with the most sent.

--- counter.py
+++ counter.py
@@ -1,8 +1,32 @@
 def countEmail():
     # This first line is provided for you
-    name = input("Enter file:")
-    if len(name) < 1 : name = "mbox-short.txt"
-    handle = open(name)
+    file_name = input("Enter file:")
+    if len(file_name) < 1 : file_name = "mbox-short.txt"
+    
+    email_sender_counts = dict() 
+
+    # Try except for bad input 
+    try:
+        with open(file_name) as file_handler: # With opens and closes file
+            for line in file_handler:         # Loops through lines in file handler
+                if line.startswith("From "):  # Finds lines that start with "From "
+                    email = line.split()[1] # Gets the second word (sender email address)
+                    
+                    # Sets it to 1 if it isn't in the dictionary and increments if it is
+                    if email not in email_sender_counts:
+                        email_sender_counts[email] = 1
+                    else:
+                        email_sender_counts[email] += 1
+
+    except:
+        print(f"Bad file name : {file_name}")
+        quit()
+
+    # Get the email with the most emails sent
+    max_email = max(email_sender_counts, key=email_sender_counts.get)
+    max_sent = email_sender_counts[max_email]
+
+    print(f"{max_email} {max_sent}")
         
 
 ## if you want to test locally run > python counter.py
